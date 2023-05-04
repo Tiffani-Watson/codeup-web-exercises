e8ffa25d86f117d8f83a0d6f8dfd54f0c2f6bfc9
Tiffani <tiffani.marie.watson@gmail.com>
2023-05-04 07:42:21 -0500

    planet assignment

--- planet-string.js
+++ planet-string.js
@@ -12,15 +12,20 @@
      * console.log planetsArray to check your work
      */
 
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split('|');
+
     console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
+
+     *
      *
      * BONUS:
-     * Create another string that would display your planets in an undordered
+     * Create another string that would display your planets in an unordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
