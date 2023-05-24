9248cb56f6af4050a2236b99096dfdd07fed9dcd
Tiffani <tiffani.marie.watson@gmail.com>
2023-05-23 23:16:23 -0500

    highlighted h1,p and li

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,3 +4,15 @@
 
 var contents = $('#social-media-links').text();
 alert(contents);
+
+
+
+"use strict"
+
+$(function(){
+    $("li").css("font-size", "20px");
+    $("h1").css("background", "yellow");
+    $("p").css("background", "yellow");
+    $("li").css("background", "yellow");
+
+});
