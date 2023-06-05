29c2a04d16cb1db8f738fa4e50d81feabd233a04
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-05 14:16:32 -0500

    added map, background, and flashing heading

--- js/weather_map.js
+++ js/weather_map.js
@@ -23,6 +23,16 @@ $(document).ready(function() {
 
         $('#weather').html(`Current temperature: ${temperatureFahrenheit}&#176;F<br>Weather: ${description}`);
     }
-
-
 });
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
