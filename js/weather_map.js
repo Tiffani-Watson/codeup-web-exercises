dd9c28475f0b7081ca3d33789137594a98c207d1
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-05 14:53:37 -0500

    adjusted code for easy flow

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,38 +1,122 @@
-$(document).ready(function() {
-    var apiKey = 'e37273f7d8371c5675f3c31021d626c5';
-    var city = 'Huntsville';
+ $(document).ready(function () {
+    let latLong = [-95.3698, 29.7604];
+    let map;
+    let marker;
 
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    function updateWeatherForecast(lat, lon) {
+    $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${MyOW_Key}`, function (data) {
+    console.log(data);
+    let weatherData = data.list;
+    let weatherClasses = [];
+    weatherData.forEach(function (item, index) {
+    if (index % 8 === 0) {
+    let weather = item.weather[0].main;
+    let weatherClass = 'default';
+    switch (weather) {
+    case 'Clear':
+    weatherClass = 'clear';
+    break;
+    case 'Clouds':
+    weatherClass = 'clouds';
+    break;
+    case 'Rain':
+    weatherClass = 'rain';
+    break;
+    // Add more cases for different weather conditions if needed
+}
+    weatherClasses.push(weatherClass);
+    let date = new Date(item.dt * 1000);
+    let dateString = date.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
+    $('.card').eq(index / 8).find('.date').text(dateString);
+}
+});
+    $('.card').each(function (index) {
+    $(this).addClass(weatherClasses[index]);
+    $(this).find('.weather-info').text(weatherData[index * 8].weather[0].description);
 
-    $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            displayWeatherData(data);
-        },
-        error: function() {
-            $('#weather').html('Error retrieving weather data.');
-        }
-    });
+    let temperatureCelsius = Math.round(weatherData[index * 8].main.temp - 273.15);
+    let temperatureFahrenheit = Math.round((temperatureCelsius * 9 / 5) + 32);
 
-    function displayWeatherData(data) {
-        var temperatureKelvin = data.main.temp;
-        var temperatureFahrenheit = Math.round((temperatureKelvin - 273.15) * 9/5 + 32);
-        var description = data.weather[0].description;
+    $(this).find('.temperature').text(temperatureFahrenheit + '\u00B0F');
+});
+});
+}
 
-        $('#weather').html(`Current temperature: ${temperatureFahrenheit}&#176;F<br>Weather: ${description}`);
-    }
+    function updateCityState(latLng) {
+    $.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${latLng[0]},${latLng[1]}.json?access_token=${MyApiKey}`, function (data) {
+    let features = data.features;
+    let city = "";
+    let state = "";
+    for (let i = 0; i < features.length; i++) {
+    if (features[i].place_type.includes('place')) {
+    city = features[i].text;
+    state = features[i].context[0].text;
+    break;
+}
+}
+    $('.card h2').text(city + ", " + state);
+});
+}
+
+    function searchLocation() {
+    var location = $('#location-input').val();
+    if (location) {
+    $.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?access_token=${MyApiKey}`, function (data) {
+    if (data.features.length > 0) {
+    var lngLat = data.features[0].geometry.coordinates;
+    latLong = [lngLat[0], lngLat[1]];
+    updateCityState(latLong);
+    updateWeatherForecast(latLong[1], latLong[0]);
+    map.flyTo({ center: latLong });
+    marker.setLngLat(latLong);
+} else {
+    alert('Location not found.');
+}
+});
+}
+}
+
+    function initializeMap() {
+    mapboxgl.accessToken = MyApiKey;
+    map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: latLong,
+    zoom: 10
 });
 
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-            // to get all the data from the request, comment out the following three lines...
-        }).then(function(data) {
-            return data.features[0].center;
-        });
+    marker = new mapboxgl.Marker({
+    draggable: true
+})
+    .setLngLat(latLong)
+    .addTo(map);
+
+    marker.on('dragend', function () {
+    var lngLat = marker.getLngLat();
+    latLong = [lngLat.lng, lngLat.lat];
+    updateCityState(latLong);
+    updateWeatherForecast(latLong[1], latLong[0]);
+});
 }
+
+    initializeMap();
+    updateWeatherForecast(latLong[1], latLong[0]);
+    updateCityState(latLong);
+
+    $('#search-btn').on('click', searchLocation);
+
+    $('#location-input').on('keyup', function (event) {
+    if (event.keyCode === 13) {
+    searchLocation();
+}
+});
+});
+
+    setInterval(function () {
+    var heading = document.getElementById('page-heading');
+    if (heading.style.color === 'black') {
+    heading.style.color = '#ff6ec7';
+} else {
+    heading.style.color = 'black';
+}
+}, 3000);
