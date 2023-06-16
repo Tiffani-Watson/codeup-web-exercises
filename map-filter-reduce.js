43af484cbbf068c1483751facfade44386658816
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-16 13:46:30 -0500

    used .reduce to get the longest email from list of users

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -14,3 +14,25 @@ const emailAdd = users.map(function(user){
 
 console.log(emailAdd);
 
+
+//# 4: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+
+const totalYearsExp = users.reduce(function(totalYears,user){
+    return totalYears + user.yearsOfExperience;
+},0)
+
+console.log(totalYearsExp);
+
+
+//# 5: Use .reduce to get the longest email from the list of users.
+
+const longestUserEmail = users.reduce(function(longestEmail,user){
+    if(user.email.length >= longestEmail.length){
+        longestEmail = user.email;
+    }
+    return longestEmail;
+},"")
+
+console.log(longestUserEmail);
+
+
