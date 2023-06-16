5208afab31f5fbe28d45fea698cb5fa822ef323c
Tiffani <tiffani.marie.watson@gmail.com>
2023-06-16 12:59:58 -0500

    completed es6 assignment

--- js/es6.js
+++ js/es6.js
@@ -26,15 +26,13 @@ const users = [
         name: 'justin',
         email: 'justin@codeup.com',
         languages: ['html', 'css', 'javascript', 'php']
-    }
+    },
     {
         name: 'Tiffani',
         email: 'tiffani.marie.watson@gmail.com',
         languages: ['javascript', 'html', 'css']
     }
 ];
-
-
 
 // TODO: fill in your name and email and add some programming languages you know
 // to the languages array
@@ -67,21 +65,35 @@ users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
-    const name = user.name;
-    const email = user.email;
-    const languages = user.languages;
+    // const name = user.name;
+    // const email = user.email;
+    // const languages = user.languages;
+
+    const {name, email, languages} = user;
 
     // TODO: rewrite the assignment below to use template strings
-    developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+//     developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+// });
+
+    developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(', ')}.`);
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+// var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+// developers.forEach(function (developer) {
+
+for (let developer of developers) {
+    // TODOne: rewrite the assignment below to use template strings
+    list += `<li>${developer}</li>`;
+}
+list += '</ul>';
 
     // TODO: rewrite the assignment below to use template strings
-    list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+//     list += '<li>' + developer + '</li>';
+// });
+// list += '</ul>';
+
+document.getElementsByTagName("body")[0].innerHTML += list;
